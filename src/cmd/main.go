7b6bcb084c3bffe3023e675998483d7ea6ca54c4
mothylag <decagufy30@gmail.com>
2026-01-18 21:43:12 -0600

    remove binary from repository

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -9,36 +9,36 @@ import (
 	"path/filepath"
 )
 
-func main(){
+func main() {
 	flag.Parse()
 	args := flag.Args()
-    if len(args) < 1 {
+	if len(args) < 1 {
 		fmt.Println("usage: pnp <route>")
-    }
+	}
 	inPath := "./"
 	outPath := "./plugs"
-    if len(args) >= 1 {
-        inPath = args[0]
-    }
-    if len(args) >= 2 {
-        outPath = args[1]
-    }
+	if len(args) >= 1 {
+		inPath = args[0]
+	}
+	if len(args) >= 2 {
+		outPath = args[1]
+	}
 
- 	absInPath,err := filepath.Abs(inPath)
+	absInPath, err := filepath.Abs(inPath)
 	if err != nil {
 		panic(err)
 	}
 
-	absOutPath,err := filepath.Abs(outPath)
+	absOutPath, err := filepath.Abs(outPath)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println("processing models at: ",absInPath)
-	fmt.Println("output dir at: ",absOutPath)
+	fmt.Println("processing models at: ", absInPath)
+	fmt.Println("output dir at: ", absOutPath)
 	tree := input.CreateTree(absInPath)
 	parsedTree := parser.CreateParser(tree)
 	parsedTree.FilterModelFiles()
 	output.ShowTree(parsedTree.Tree)
-	parsedTree.GetEntities()
+	//parsedTree.GetEntities()
 }
