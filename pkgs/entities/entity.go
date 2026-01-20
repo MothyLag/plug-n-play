6b1ce4a1fd0f3390727cb35ddd5fd35cd48398b7
mothylag <decagufy30@gmail.com>
2026-01-20 01:59:30 -0600

    parse Struct name, Fields and types into Entity object

--- pkgs/entities/entity.go
+++ pkgs/entities/entity.go
@@ -1,14 +1,30 @@
 package entities
 
 type Entity struct {
-	Name string
-	Fields []Field
+	Name      string
+	Fields    []Field
 	DependsOn []string
 }
 
-type Field struct{
+type Field struct {
 	Name string
 	Type string
 }
 
+type EntitiesTree []Entity
 
+func CreateEntitiesTree() EntitiesTree {
+	return []Entity{}
+}
+
+func NewEntity(name string, fields []Field) Entity {
+	return Entity{
+		Name:      name,
+		Fields:    fields,
+		DependsOn: []string{},
+	}
+}
+
+func (e *EntitiesTree) AppendEntity(newEntity Entity) {
+	*e = append(*e, newEntity)
+}
--- pkgs/parser/go.parser.go
+++ pkgs/parser/go.parser.go
@@ -5,16 +5,16 @@ import (
 	"mothylag/pnp/pkgs/entities"
 	"mothylag/pnp/pkgs/input"
 	"os"
+	"regexp"
 	"strings"
 )
 
 type GoParser struct {
-	Tree     *input.Tree
-	Entities []entities.Entity
+	Tree *input.Tree
 }
 
 func CreateParser(t *input.Tree) *GoParser {
-	return &GoParser{Tree: t, Entities: []entities.Entity{}}
+	return &GoParser{Tree: t}
 }
 
 func (p *GoParser) FilterModelFiles() {
@@ -38,8 +38,33 @@ func (p *GoParser) GetContent() {
 	}
 }
 
-func parseEntity(content string) {
+func parse2Entity(content string) entities.EntitiesTree {
+	tree := entities.CreateEntitiesTree()
+	//pattern to get entity name and content into brackets block
+	pattern := `(?:type)\s+(?P<Entity>\w+)\s+(?:struct)(?:\s*)(?:\n*)(?:{)(?:\n*)(?P<Content>[^}]*)`
+	re := regexp.MustCompile(pattern)
+	matches := re.FindAllStringSubmatch(content, -1)
+	for _, match := range matches {
+		entityName := match[1]
+		fields := parseFields(match[2])
+		entity := entities.NewEntity(entityName, fields)
+		tree.AppendEntity(entity)
+	}
+	return tree
+}
 
+func parseFields(content string) []entities.Field {
+	fields := []entities.Field{}
+	//pattern to get field name and type from content
+	pattern := `(?P<Key>\w+)\s+(?P<Value>\w+);`
+	re := regexp.MustCompile(pattern)
+	matches := re.FindAllStringSubmatch(content, -1)
+	for _, match := range matches {
+		key := match[1]
+		value := match[2]
+		fields = append(fields, entities.Field{Name: key, Type: value})
+	}
+	return fields
 }
 
 func isEntityFile(f string) bool {
